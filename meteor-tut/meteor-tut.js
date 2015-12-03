a2a386838bceb7286508114a0eaf8cb0adec4465
robocopatrick <robocopatrick@gmail.com>
2015-12-03 18:59:17 +0100

    Video-3: Views & Templates
    Views & Templates
    https://www.youtube.com/watch?v=nF5CRSEC8PA&index=5&list=PLLnpHn493BHECN
    l9I8gwos-hEfFrer7TV

--- meteor-tut/meteor-tut.js
+++ meteor-tut/meteor-tut.js
@@ -1,18 +1,12 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.body.helpers({
+    resolutions: [
+      { title: "#1" },
+      { title: "#2" },
+      { title: "#3" },
+      { title: "#4" },
+      { title: "#5" }
+    ]
   });
 }
 
